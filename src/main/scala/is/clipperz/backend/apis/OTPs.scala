7c27911431a4d1b606cfb7d78f43f98934762a96
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-12-04 21:02:07 +0100

    Implement otp apis

--- src/main/scala/is/clipperz/backend/apis/OTPs.scala
+++ src/main/scala/is/clipperz/backend/apis/OTPs.scala
@@ -3,6 +3,7 @@ package is.clipperz.backend.apis
 import java.io.FileNotFoundException
 import zio.ZIO
 import zio.stream.ZStream
+import zio.json.EncoderOps
 import zhttp.http.{ Headers, HeaderNames, HeaderValues, Http, Body, Method, Path, PathSyntax, Response, Status }
 import zhttp.http.* //TODO: fix How do you import `!!` and `/`?
 import is.clipperz.backend.data.HexString
@@ -23,6 +24,7 @@ import is.clipperz.backend.services.OTPArchive
 import is.clipperz.backend.services.SaveOTPBlobData
 import is.clipperz.backend.services.SessionManager
 import is.clipperz.backend.services.SaveUsedOTPBlobData
+import is.clipperz.backend.exceptions.NonAuthorizedException
 
 val otpsApi: ClipperzHttpApp = Http.collectZIO {
   case request @ Method.POST -> !! / "otps" / hash =>
@@ -35,14 +37,14 @@ val otpsApi: ClipperzHttpApp = Http.collectZIO {
       )
       .map(results => Response.text(s"${results}"))
       .catchSome {
-        // case ex: EmptyContentException =>
-        //   ZIO.logWarningCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
-        // case ex: BadRequestException =>
-        //   ZIO.logWarningCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
-        // case ex: NonWritableArchiveException =>
-        //   ZIO.logFatalCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.InternalServerError))
-        // case ex: FailedConversionException =>
-        //   ZIO.logWarningCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
+        case ex: EmptyContentException =>
+          ZIO.logWarningCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
+        case ex: BadRequestException =>
+          ZIO.logWarningCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
+        case ex: NonWritableArchiveException =>
+          ZIO.logFatalCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.InternalServerError))
+        case ex: FailedConversionException =>
+          ZIO.logWarningCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
         case ex => ZIO.logFatalCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.InternalServerError))
       } @@ LogAspect.logAnnotateRequestData(request)
 
@@ -72,6 +74,7 @@ val otpsApi: ClipperzHttpApp = Http.collectZIO {
           .map(b => if b then Response.ok else Response(status = Status.NotFound))
       )
       .catchSome {
+        case ex: NonAuthorizedException => ZIO.succeed(Response(status = Status.Forbidden))
         case ex: ResourceNotFoundException => ZIO.succeed(Response(status = Status.NotFound))
         case ex: BadRequestException =>
           ZIO.logInfoCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
@@ -83,6 +86,36 @@ val otpsApi: ClipperzHttpApp = Http.collectZIO {
       } @@ LogAspect.logAnnotateRequestData(request) 
 
   case request @ Method.GET -> !! / "otps" / hash =>
-    ZIO.succeed(Response(status = Status.NotImplemented)) @@ LogAspect.logAnnotateRequestData(request)
+    ZIO
+      .service[OTPArchive]
+      // .zip(ZIO.service[SessionManager])
+      .zip(ZIO.succeed(request.body.asStream))
+      .flatMap((otpArchive, /* sessionManager, */ content) =>
+        val verifiers = request.url.queryParams.get("verifier")
+        verifiers match
+          case None => ZIO.fail(new BadRequestException("No verifier sent"))
+          case Some(ls) =>
+            if ls.length == 1 then
+              otpArchive
+                .getOTPBlob(HexString(hash), HexString(ls(0)))
+                .map(b =>
+                  b match
+                    case Left(o) => Response.json(o.toJson)
+                    case Right(o) => Response.json(o.toJson)
+                )
+            else 
+              ZIO.fail(new BadRequestException("No verifier sent"))
+      )
+      .catchSome {
+        case ex: NonAuthorizedException => ZIO.succeed(Response(status = Status.Forbidden))
+        case ex: ResourceNotFoundException => ZIO.succeed(Response(status = Status.NotFound))
+        case ex: BadRequestException =>
+          ZIO.logInfoCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
+        case ex: NonWritableArchiveException =>
+          ZIO.logFatalCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.InternalServerError))
+        case ex: FailedConversionException =>
+          ZIO.logWarningCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.BadRequest))
+        case ex => ZIO.logFatalCause(s"${ex.getMessage()}", Cause.fail(ex)).as(Response(status = Status.InternalServerError))
+      } @@ LogAspect.logAnnotateRequestData(request) 
 
 }
